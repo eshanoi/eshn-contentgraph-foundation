ad0a0c2ed8b01250156a704827dc2b196fd54696
Dung.Nguyen <dung.nguyentien@optimizely.com>
2025-07-09 15:25:27 +0700

    fix: Remove redundant fragment

--- src/components/pages/LandingPage/index.tsx
+++ src/components/pages/LandingPage/index.tsx
@@ -5,35 +5,34 @@ export default function LandingPage({ page }: { page: LandingPageFragment }) {
   const { MainBody, MainContentArea, TopContentArea, Margin, Padding } = page;
 
   return (
-      <div
-        className={`landing-page row no-gutters ${Margin || ''} ${Padding || ''}`}
-      >
-        {/* Top Content Area */}
-        <div className="col-12">
-          {TopContentArea && (
-            <div className="col-12">
-              <ContentArea contentArea={TopContentArea} />
-            </div>
-          )}
-        </div>
-
-        {/* Main Body Content */}
-        {MainBody && (
+    <div
+      className={`landing-page row no-gutters ${Margin || ''} ${Padding || ''}`}
+    >
+      {/* Top Content Area */}
+      <div className="col-12">
+        {TopContentArea && (
           <div className="col-12">
-            <div
-              className="main-body-content"
-              dangerouslySetInnerHTML={{ __html: MainBody }}
-            />
-          </div>
-        )}
-
-        {/* Main Content Area */}
-        {MainContentArea && (
-          <div className="col-12">
-            <ContentArea contentArea={MainContentArea} />
+            <ContentArea contentArea={TopContentArea} />
           </div>
         )}
       </div>
-    </>
+
+      {/* Main Body Content */}
+      {MainBody && (
+        <div className="col-12">
+          <div
+            className="main-body-content"
+            dangerouslySetInnerHTML={{ __html: MainBody }}
+          />
+        </div>
+      )}
+
+      {/* Main Content Area */}
+      {MainContentArea && (
+        <div className="col-12">
+          <ContentArea contentArea={MainContentArea} />
+        </div>
+      )}
+    </div>
   );
 }
